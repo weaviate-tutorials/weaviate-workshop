7cd1710cd26f361b3ef9a3389ec14d556ebbaea7
JP Hwang <me@jphwang.com>
2024-03-15 18:54:35 +0000

    wip

--- app_ui/app.py
+++ app_ui/app.py
@@ -13,82 +13,40 @@ headers = {
     ),  # Replace with your inference API key
 }
 
-with st.expander("Weaviate connection details"):
-    conn_type = st.selectbox(
-        "Weaviate connection type", options=["WCS", "Local"], index=0
-    )
-    if conn_type == "WCS":
-        use_env_vars = st.checkbox("Use environment variable keys", value=True)
-        if use_env_vars:
-            url_envvar = st.text_input(
-                label="Environment key for the URL",
-                key="url_env_var",
-                value="WORKSHOP_DEMO_URL",
-            )
-            key_envvar = st.text_input(
-                label="Environment key for the API key",
-                key="key_env_var",
-                value="WORKSHOP_DEMO_KEY_ADMIN",
-            )
-            cluster_url = os.getenv(url_envvar)
-            cluster_key = os.getenv(key_envvar)
-        else:
-            cluster_url = st.text_input(
-                label="URL", key="cluster_url", value="YOUR_CLUSTER_URL"
-            )
-            cluster_key = st.text_input(
-                label="API key", key="cluster_key", value="YOUR_CLUSTER_API_KEY"
-            )
-
-        # Connect to Weaviate
-        try:
-            client = weaviate.connect_to_wcs(
-                cluster_url=cluster_url,
-                auth_credentials=weaviate.auth.AuthApiKey(cluster_key),
-                headers=headers,
-            )
-        except Exception as e:
-            client = None
-            st.error(f"Error connecting to Weaviate: {e}")
+conn_type = st.selectbox(
+    "Weaviate connection type", options=["WCS", "Local"], index=0
+)
+if conn_type == "WCS":
+    client_url = os.getenv("WORKSHOP_DEMO_URL")
+    client_key = os.getenv("WORKSHOP_DEMO_KEY_ADMIN")
+    if client_url is None or client_key is None:
+        client = None
     else:
-        try:
-            client = weaviate.connect_to_local(headers=headers)
-        except Exception as e:
-            client = None
-            st.error(f"Error connecting to Weaviate: {e}")
-
+        client = weaviate.connect_to_wcs(
+            cluster_url=os.getenv("WORKSHOP_DEMO_URL"),
+            auth_credentials=weaviate.auth.AuthApiKey(os.getenv("WORKSHOP_DEMO_KEY_ADMIN")),
+            headers=headers,
+        )
+else:
+    client = weaviate.connect_to_local(
+        headers=headers
+    )
 
 # Main app
-if client is not None:
+if client is None:
+    st.write("No Weaviate client found. Check connection details.")
+else:
     st.header("Weaviate Search and RAG Demo")
     st.subheader("Inputs")
 
-    input_l, input_r = st.columns(2, gap="small")
+    collection = client.collections.get("Wikipedia")
+    main_column = "title"
 
-    with input_l:
-        with st.container(height=200):
-            collections = client.collections.list_all(simple=True)
-            collection_name = st.selectbox(
-                label="Collection name",
-                options=[c for c in sorted(collections.keys())],
-                key="collection_name",
-                index=0,
-            )
-            collection = client.collections.get(collection_name)
-            random_object = collection.query.fetch_objects(limit=1)
-            property_names = sorted(random_object.objects[0].properties.keys())
-            main_column = st.selectbox(
-                label="Summary column",
-                options=[p for p in property_names],
-                key="summary_column",
-                index=0,
-            )
-    with input_r:
-        with st.container(height=200):
-            query_str = st.text_input(
-                label="Query string", value="fashion sneakers", key="query_str"
-            )
-            limit = st.number_input(label="Limit", value=10, key="limit")
+    with st.container(height=200):
+        query_str = st.text_input(
+            label="Query string", value="how do planes fly", key="query_str"
+        )
+        limit = st.number_input(label="Limit", value=7, key="limit")
 
     return_metadata = MetadataQuery(distance=True)
 
